bf405f4737997654b5b377f91bedcebab0a99a0a
casuzu <66433868+casuzu@users.noreply.github.com>
2022-05-15 17:48:57 -0400

    Delete chess.py

--- chess.py
+++
@@ -1,8 +0,0 @@
-
-#chess file module
-import tkinter as tk
-
-def call_chess(canvas):
-    print("Hello World from planet Chess")
-    canvas.delete('all')
-    canvas.create_text(100, 100, text="Hello World from planet Chess", fill='red')
